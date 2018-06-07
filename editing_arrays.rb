9f197259812896826008436902530373c71640de
rosewhitt <rosewhitt@icloud.com>
2018-06-07 13:29:02 +0000

    Done.

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -1,11 +1,12 @@
 rainbow_colors = ["yellow", "default", "light_cyan"]
 
-
-
 def change_rainbow_colors
-  rainbow_colors[0] = 'red'
-
-  rainbow_colors[1] = 'light_red'
-
-  rainbow_colors[2] = 'light_yellow'
+  rainbow_colors[0] = "red"
+    puts rainbow_colors[0]
+  rainbow_colors[1] = "light_red"
+    puts rainbow_colors[1]
+  rainbow_colors[2] = "light_yellow"
+    puts rainbow_colors[2]
 end
+
+puts change_rainbow_colors
